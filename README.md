3ded575a4394e6619f6bb9920d3598bca6a2d2b9
Ishiita_Pal <ishiitapal@gmail.com>
2024-04-20 22:22:43 +0200

    mango mate app

---
+++ README.md
@@ -0,0 +1,9 @@
+# MangoMate! - A Querky Social Media App
+
+*Our motto: "Send a Mango, Make 'Em Glow!"*
+
+## Client 
+
+## Server
+
+## AI Model
---
+++ server/routes/auth.js
@@ -0,0 +1,9 @@
+//server/routes/auth.js
+import express from "express";
+import { login } from "../controllers/auth.js";
+
+const router = express.Router();
+
+router.post("/login", login);
+
+export default router;
